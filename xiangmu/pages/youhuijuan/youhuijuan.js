05e710bbb0a8cca7fa1976c7c70f92d9b4897225
xuebaobao <2890469125@qq.com>
2019-07-02 16:47:05 +0800

    小程序哦

---
+++ xiangmu/pages/youhuijuan/youhuijuan.js
@@ -0,0 +1,80 @@
+// pages/youhuijuan/youhuijuan.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    neirong:[
+      {
+        text:"此处写名字,字体是思源黑体"
+      },
+      {
+        text: "此处写名字,字体是思源黑体"
+      },
+      {
+        text: "此处写名字,字体是思源黑体"
+      },
+    ],
+    hidden:true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  //查看详情
+  chakanxiangqing:function(e){
+
+  }, 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
