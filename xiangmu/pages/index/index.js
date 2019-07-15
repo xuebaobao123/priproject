7f1e85cbd00b3a9e62aa5f3ee3187f011bc32f49
xuebaobao <2890469125@qq.com>
2019-07-15 15:46:48 +0800

    参伙

--- xiangmu/pages/index/index.js
+++ xiangmu/pages/index/index.js
@@ -1,4 +1,5 @@
-const app = getApp()
+const app = getApp();
+var util = require('../../utils/fengzhuang.js');
 Page({
   data: {
     indicatorDots: true,  //是否显示面板指示点
@@ -22,6 +23,11 @@ Page({
     ]
   },
   onLoad: function () {
+    var e = wx.getStorageSync('e');
+    this.setData({
+      shouquan: !!e,
+    })
+    this.initToken();
   },
   //点击跳转
   foward: function (e) {
@@ -30,5 +36,56 @@ Page({
     wx.navigateTo({
       url: advertPlace.fowardUrl
     })
-  }
+  },
+  initToken:function(){
+    var e = wx.getStorageSync('e');
+  
+    wx.login({
+      success(res) {
+        if (res.code) {
+          var data={
+            merchants_id:"1",
+            merchants_id:res.code
+          }
+          util.postRequest(app.globalData.url + "auth/openid",data)
+            .then(function (data) {
+              util.postRequest(app.globalData.url + "auth/login", { openId: data})
+                .then(function (tokenData) {
+                  if (tokenData.status == "0") {
+                    //成功获取token
+                    wx.setStorageSync("e",{...e,accessToken:tokenData})
+                  }
+                }, function (error) {
+                })
+            }, function (error) {
+            })
+        }
+      }
+    })
+  },
+  // 授权登陆
+  bindGetUserInfo: function (e) {
+    wx.setStorageSync('e', e.detail.userInfo);
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      var that = this;
+      that.setData({
+        shouquan: true,
+      });
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        showCancel: false,
+        confirmText: '返回授权',
+        success: function (res) {
+          // 用户没有授权成功，不需要改变 isHide 的值
+          if (res.confirm) {
+            console.log('用户点击了“返回授权”');
+          }
+        }
+      });
+    }
+  },
 })
